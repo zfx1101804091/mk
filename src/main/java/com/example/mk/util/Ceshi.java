9936445f1eedcb4ed9854b7611ffd34d57dafcbd
zfx1101804091 <1875586562@qq.com>
2019-11-12 14:00:05 +0800

    2019-11-12 13:59:58

--- src/main/java/com/example/mk/util/Ceshi.java
+++ src/main/java/com/example/mk/util/Ceshi.java
@@ -1,9 +1,17 @@
 package com.example.mk.util;
 
+import com.alibaba.fastjson.JSONArray;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * String.format()的详细用法
@@ -13,11 +21,11 @@ public class Ceshi {
 
 
     public static void main(String[] args) {
-        System.out.println(String.format("%07d", 7));
+//        System.out.println(String.format("%07d", 7));
 
 // Output: 0000007
 
-        String str = String.format("Hi,%s %s %s", "小超", "是个", "大帅哥");
+      /*  String str = String.format("Hi,%s %s %s", "小超", "是个", "大帅哥");
         System.out.println(str);
 
 
@@ -41,6 +49,44 @@ public class Ceshi {
         String operation = "window10";
         String date1 = DateUtil.getNowDate();
         String msg = "{\"ip\":"+ip+",\"operation\":"+operation+",\"browser\":"+browser+",\"editime\":"+date1+"}";
-        System.out.println("msg--"+msg);
+        System.out.println("msg--"+msg);*/
+
+
+        List<String> ipList = getLocalIPList();
+        for (String s : ipList) {
+            System.out.println(s);
+        }
+
+    }
+
+    /**
+     * IceWee 2013.07.19
+     * 获取本地IP列表（针对多网卡情况）
+     *
+     * @return
+     */
+    public static List<String> getLocalIPList() {
+        List<String> ipList = new ArrayList<String>();
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface networkInterface;
+            Enumeration<InetAddress> inetAddresses;
+            InetAddress inetAddress;
+            String ip;
+            while (networkInterfaces.hasMoreElements()) {
+                networkInterface = networkInterfaces.nextElement();
+                inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    inetAddress = inetAddresses.nextElement();
+                    if (inetAddress != null && inetAddress instanceof Inet4Address) { // IPV4
+                        ip = inetAddress.getHostAddress();
+                        ipList.add(ip);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ipList;
     }
 }
